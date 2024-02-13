367eb057272ddb0f92e3a0dad08cde00c0d23c7f
YasirActualy <yassir032003@example.com>
2024-02-13 16:27:33 -0500

    side bard

--- index.js
+++ index.js
@@ -1,32 +1,75 @@
 window.onload = function(){
-    slideOne();
-    slideTwo();
+  slideOne();
+  slideTwo();
+  slideThree();
+  slideFour();
 }
 let sliderOne = document.getElementById("slider-1");
 let sliderTwo = document.getElementById("slider-2");
+let sliderThree = document.getElementById("slider-3");
+let sliderFour = document.getElementById("slider-4");
+
 let displayValOne = document.getElementById("range1");
 let displayValTwo = document.getElementById("range2");
+let displayValThree = document.getElementById("range3");
+let displayValFour = document.getElementById("range4");
+
 let minGap = 0;
-let sliderTrack = document.querySelector(".slider-track");
+let sliderTrack = document.getElementById("track1");
+let sliderTrack2 = document.getElementById("track2");
 let sliderMaxValue = document.getElementById("slider-1").max;
+let sliderMaxValue2 = document.getElementById("slider-3").max;
+
+// controls the left slider
 function slideOne(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-        sliderOne.value = parseInt(sliderTwo.value) - minGap;
-    }
-    displayValOne.textContent = sliderOne.value;
-    fillColor();
+  if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+    // stops the sliders in case the gap is less than the minimum gap
+      sliderOne.value = parseInt(sliderTwo.value) - minGap;
+  }
+  displayValOne.textContent = sliderOne.value;
+  fillColor();
 }
+//controls the right slider
 function slideTwo(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-        sliderTwo.value = parseInt(sliderOne.value) + minGap;
-    }
-    displayValTwo.textContent = sliderTwo.value;
-    fillColor();
+  if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+      sliderTwo.value = parseInt(sliderOne.value) + minGap;
+  }
+  displayValTwo.textContent = sliderTwo.value;
+  fillColor();
 }
+
+
+// controls the left slider
+function slideThree(){
+  if(parseInt(sliderFour.value) - parseInt(sliderThree.value) <= minGap){
+    // stops the sliders in case the gap is less than the minimum gap
+      sliderThree.value = parseInt(sliderFour.value) - minGap;
+  }
+  displayValThree.textContent = sliderThree.value;
+  fillColor2();
+}
+
+//controls the right slider
+function slideFour(){
+  if(parseInt(sliderFour.value) - parseInt(sliderThree.value) <= minGap){
+      sliderFour.value = parseInt(sliderThree.value) + minGap;
+  }
+  displayValFour.textContent = sliderFour.value;
+  fillColor2();
+  // prints done
+  console.log("done");
+}
+
 function fillColor(){
-    percent1 = (sliderOne.value / sliderMaxValue) * 100;
-    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
-    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+  percent1 = (sliderOne.value / sliderMaxValue) * 100;
+  percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+  sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+}
+
+function fillColor2(){
+    percent1 = (sliderThree.value / sliderMaxValue2) * 100;
+    percent2 = (sliderFour.value / sliderMaxValue2) * 100;
+    sliderTrack2.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
 }
 
 
