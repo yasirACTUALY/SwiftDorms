2648e6b293b75557e71cb07b801029a7062f0002
serialized-kirin <serialized.kirin@protonmail.com>
2024-10-25 23:27:53 -0400

    server edits, along with documentation of how to setup https on a server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,28 @@
 import {resolve as pathResolve} from 'node:path'
 import {createServer} from 'node:http'
 import {readFileSync} from 'node:fs'
-// import {default as mysql} from 'mysql'
+import {default as mysql} from 'mysql2'
 
+
+const db = mysql.createConnection({
+  host: process.env[DB_HOST] || 'localhost',
+  user: process.env[DB_USER] || 'root',
+  password: process.env[DB_PASS] || 'root',
+  database: process.env[DB_DATABASE] || 'mydatabase',
+});
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+  } else {
+    console.log('Connected to MySQL');
+  }
+});
+//                                      for paging
+//                                  vvvvvvvvvvvvvvvvvvv
+async function getListings(keyword/*begin_index, amount*/) {
+  let res = db.query('SELECT * FROM listings WHERE name LIKE ?', [keyword]);
+  // res.
+}
 
 // want to know exactly what we are giving the user access to.
 // We should better organize our source files and static web files before this can be done in
